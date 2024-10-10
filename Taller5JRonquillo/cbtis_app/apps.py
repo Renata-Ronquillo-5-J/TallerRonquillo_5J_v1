9481b8a72ee4ebbb48a066364fb4cf3e043abe4a
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-10-10 12:04:41 -0600

    Delete Taller5JRonquillo directory

--- Taller5JRonquillo/cbtis_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CbtisAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cbtis_app'
--- Taller5JRonquillo/cbtis_app/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from cbtis_app import views
-urlpatterns = [
-    path('',views.ver_lista,name='ver_lista')
-]
